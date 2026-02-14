f003cb8e0ca986d012c535f60fcbdb34afadf96f
Matthew King <matt@valiantsoftware.com.au>
2026-02-14 21:24:58 +0800

    Update to latest language version, package versions, etc.

--- test/FluentFileFilterBuilder.Tests/FileFilterBuilderTests.cs
+++ test/FluentFileFilterBuilder.Tests/FileFilterBuilderTests.cs
@@ -3,97 +3,96 @@ using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
-namespace FluentFileFilterBuilder.Tests
+namespace FluentFileFilterBuilder.Tests;
+
+public class FileFilterBuilderTests
 {
-    public class FileFilterBuilderTests
+    [Fact]
+    public void Add_DescriptionIsNull_ThrowsArgumentNullException()
     {
-        [Fact]
-        public void Add_DescriptionIsNull_ThrowsArgumentNullException()
+        FileFilterBuilder builder = new FileFilterBuilder();
+        Action act = () => builder.Add(null, "ext");
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Add_DescriptionIsEmpty_ThrowsArgumentException()
+    {
+        FileFilterBuilder builder = new FileFilterBuilder();
+        Action act = () => builder.Add(String.Empty, "ext");
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Add_ExtensionsIsNull_ThrowsArgumentNullException()
+    {
+        FileFilterBuilder builder = new FileFilterBuilder();
+        Action act = () => builder.Add("description", null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Add_ExtensionsIsEmpty_ThrowsArgumentException()
+    {
+        FileFilterBuilder builder = new FileFilterBuilder();
+        Action act = () => builder.Add("ext", Array.Empty<string>());
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCaseData))]
+    public void ToString_ReturnsExpectedValues(FileFilterBuilder builder, string expected)
+    {
+        builder.ToString().Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCaseData))]
+    public void ImplicitStringOperator_ReturnsExpectedValues(FileFilterBuilder builder, string expected)
+    {
+        string s = builder;
+        s.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> TestCaseData()
+    {
+        // Single filter, single extension.
+        yield return new object[]
         {
-            FileFilterBuilder builder = new FileFilterBuilder();
-            Action act = () => builder.Add(null, "ext");
+            new FileFilterBuilder().Add("All files", "*"),
+            "All files (*.*)|*.*"
+        };
 
-            act.Should().Throw<ArgumentNullException>();
-        }
-
-        [Fact]
-        public void Add_DescriptionIsEmpty_ThrowsArgumentException()
+        // Single filter, multiple extensions.
+        yield return new object[]
         {
-            FileFilterBuilder builder = new FileFilterBuilder();
-            Action act = () => builder.Add(String.Empty, "ext");
+            new FileFilterBuilder().Add("Image files", "bmp", "jpg", "gif"),
+            "Image files (*.bmp;*.jpg;*.gif)|*.bmp;*.jpg;*.gif"
+        };
 
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [Fact]
-        public void Add_ExtensionsIsNull_ThrowsArgumentNullException()
+        // Single filter, single extension, dot in extension.
+        yield return new object[]
         {
-            FileFilterBuilder builder = new FileFilterBuilder();
-            Action act = () => builder.Add("description", null);
+            new FileFilterBuilder().Add("All files", ".*"),
+            "All files (*.*)|*.*"
+        };
 
-            act.Should().Throw<ArgumentNullException>();
-        }
-
-        [Fact]
-        public void Add_ExtensionsIsEmpty_ThrowsArgumentException()
+        // Multiple filters, single extension.
+        yield return new object[]
         {
-            FileFilterBuilder builder = new FileFilterBuilder();
-            Action act = () => builder.Add("ext", Array.Empty<string>());
+            new FileFilterBuilder().Add("Text files", "txt").Add("All files", "*"),
+            "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        };
 
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [Theory]
-        [MemberData(nameof(TestCaseData))]
-        public void ToString_ReturnsExpectedValues(FileFilterBuilder builder, string expected)
+        // Multiple filters, multiple extensions
+        yield return new object[]
         {
-            builder.ToString().Should().Be(expected);
-        }
-
-        [Theory]
-        [MemberData(nameof(TestCaseData))]
-        public void ImplicitStringOperator_ReturnsExpectedValues(FileFilterBuilder builder, string expected)
-        {
-            string s = builder;
-            s.Should().Be(expected);
-        }
-
-        public static IEnumerable<object[]> TestCaseData()
-        {
-            // Single filter, single extension.
-            yield return new object[]
-            {
-                new FileFilterBuilder().Add("All files", "*"),
-                "All files (*.*)|*.*"
-            };
-
-            // Single filter, multiple extensions.
-            yield return new object[]
-            {
-                new FileFilterBuilder().Add("Image files", "bmp", "jpg", "gif"),
-                "Image files (*.bmp;*.jpg;*.gif)|*.bmp;*.jpg;*.gif"
-            };
-
-            // Single filter, single extension, dot in extension.
-            yield return new object[]
-            {
-                new FileFilterBuilder().Add("All files", ".*"),
-                "All files (*.*)|*.*"
-            };
-
-            // Multiple filters, single extension.
-            yield return new object[]
-            {
-                new FileFilterBuilder().Add("Text files", "txt").Add("All files", "*"),
-                "Text files (*.txt)|*.txt|All files (*.*)|*.*"
-            };
-
-            // Multiple filters, multiple extensions
-            yield return new object[]
-            {
-                new FileFilterBuilder().Add("Image files", "bmp", "jpg", "gif").Add("All files", "*"),
-                "Image files (*.bmp;*.jpg;*.gif)|*.bmp;*.jpg;*.gif|All files (*.*)|*.*"
-            };
-        }
+            new FileFilterBuilder().Add("Image files", "bmp", "jpg", "gif").Add("All files", "*"),
+            "Image files (*.bmp;*.jpg;*.gif)|*.bmp;*.jpg;*.gif|All files (*.*)|*.*"
+        };
     }
 }
--- test/FluentFileFilterBuilder.Tests/FileFilterItemTests.cs
+++ test/FluentFileFilterBuilder.Tests/FileFilterItemTests.cs
@@ -3,71 +3,70 @@ using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
-namespace FluentFileFilterBuilder.Tests
+namespace FluentFileFilterBuilder.Tests;
+
+public class FileFilterItemTests
 {
-    public class FileFilterItemTests
+    [Fact]
+    public void Constructor_DescriptionIsNull_ThrowsArgumentNullException()
     {
-        [Fact]
-        public void Constructor_DescriptionIsNull_ThrowsArgumentNullException()
+        Action act = () => new FileFilterItem(null, new string[] { "ext" });
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Constructor_DescriptionIsEmpty_ThrowsArgumentException()
+    {
+        Action act = () => new FileFilterItem(String.Empty, new string[] { "ext" });
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Constructor_ExtensionsIsNull_ThrowsArgumentNullException()
+    {
+        Action act = () => new FileFilterItem("description", null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Constructor_ExtensionsIsEmpty_ThrowsArgumentException()
+    {
+        Action act = () => new FileFilterItem("description", Array.Empty<string>());
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCaseData))]
+    public void ToString_ReturnsExpectedValues(object item, string expected)
+    {
+        ((FileFilterItem)item).ToString().Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> TestCaseData()
+    {
+        // Single extension.
+        yield return new object[]
         {
-            Action act = () => new FileFilterItem(null, new string[] { "ext" });
+            new FileFilterItem("All files", new string[] { "*" }),
+            "All files (*.*)|*.*"
+        };
 
-            act.Should().Throw<ArgumentNullException>();
-        }
-
-        [Fact]
-        public void Constructor_DescriptionIsEmpty_ThrowsArgumentException()
+        // Multiple extensions.
+        yield return new object[]
         {
-            Action act = () => new FileFilterItem(String.Empty, new string[] { "ext" });
+            new FileFilterItem("Image files", new string[] { "bmp", "jpg", "gif" }),
+            "Image files (*.bmp;*.jpg;*.gif)|*.bmp;*.jpg;*.gif"
+        };
 
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [Fact]
-        public void Constructor_ExtensionsIsNull_ThrowsArgumentNullException()
+        // Dot in extension.
+        yield return new object[]
         {
-            Action act = () => new FileFilterItem("description", null);
-
-            act.Should().Throw<ArgumentNullException>();
-        }
-
-        [Fact]
-        public void Constructor_ExtensionsIsEmpty_ThrowsArgumentException()
-        {
-            Action act = () => new FileFilterItem("description", Array.Empty<string>());
-
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [Theory]
-        [MemberData(nameof(TestCaseData))]
-        public void ToString_ReturnsExpectedValues(object item, string expected)
-        {
-            ((FileFilterItem)item).ToString().Should().Be(expected);
-        }
-
-        public static IEnumerable<object[]> TestCaseData()
-        {
-            // Single extension.
-            yield return new object[]
-            {
-                new FileFilterItem("All files", new string[] { "*" }),
-                "All files (*.*)|*.*"
-            };
-
-            // Multiple extensions.
-            yield return new object[]
-            {
-                new FileFilterItem("Image files", new string[] { "bmp", "jpg", "gif" }),
-                "Image files (*.bmp;*.jpg;*.gif)|*.bmp;*.jpg;*.gif"
-            };
-
-            // Dot in extension.
-            yield return new object[]
-            {
-                new FileFilterItem("All files", new string[] { ".*" }),
-                "All files (*.*)|*.*"
-            };
-        }
+            new FileFilterItem("All files", new string[] { ".*" }),
+            "All files (*.*)|*.*"
+        };
     }
 }
